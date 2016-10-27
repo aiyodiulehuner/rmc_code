b0417106da16edada1297c10e5890472609c9a1d
Suriya Gunasekar <suriya@hypatia.local>
2016-10-26 19:16:50 -0500

    movielens1

--- movielens.m
+++ movielens.m
@@ -10,23 +10,23 @@ par.tol     = 1e-3;
 par.maxiter = 50;
 par.verbose = 1;
 f={'spearman_rho', 'kendall_tau', 'NDCG', 'MSE', 'Precision'};
-niter=1;
+
 
 par.maxrank=1000;
-cviter=1;
+cviter=5;
 par.nnp = 1;
 RMC=0;
 SMC=1;
 K=5;
 th=3;
 if (RMC)
-muiter=[1,5,10,20,50,100,250,500,1000];    
-resultRMC=zeros(niter,cviter, length(muiter), 3, length(f));
-mu0=1;%sum(svd(Y));
-for i=1:length(niter)
+    muiter=[1,5,10,20,50,100,250,500,1000];    
+    resultRMC=zeros(cviter, length(muiter), 3, length(f));
+    mu0=1;%sum(svd(Y));
+
     for ci=1:length(cviter)
         cv=cviter(ci);
-        load(sprintf('../ml-100k/folds/ml_a.mat'));
+        load(sprintf('../ml-100k/folds/ml_%d.mat',ci));
         par.maxrank = min([d1,d2,par.maxrank]);
         fprintf('Size: %dX%d, p:%f, train:val:test::%d:%d:%d\n',d1,d2,cv,...
             length(yy),length(yy_val),length(yy_test));
@@ -43,36 +43,34 @@ for i=1:length(niter)
             t=toc;
             yest=Amap_MatComp(Yrmc,ii,Jcol);            
             k1=evalRanking(yy,yest,Jcol,f,K,th);            
-            resultRMC(i,ci,m,1,:)=k1;
+            resultRMC(ci,m,1,:)=k1;
             
             %validation            
             yest_val=Amap_MatComp(Yrmc,ii_val,Jcol_val);
             k2=evalRanking(yy_val,yest_val,Jcol_val,f,K,th);
-            resultRMC(i,ci,m,2,:)=k2;
+            resultRMC(ci,m,2,:)=k2;
                         
             %testing
             yest_test=Amap_MatComp(Yrmc,ii_test,Jcol_test);
             k3=evalRanking(yy_test,yest_test,Jcol_test,f,K,th);                
-            resultRMC(i,ci,m,3,:)= k3;  
+            resultRMC(ci,m,3,:)= k3;  
             
             fprintf('RMC  mu:%f. iter:%d, res:%f, ||X||_*:%f, t:%f\n', ...
                 mu, iter,res,sum(sum(Yrmc.U.^2)),t);
             save('ml_resultRMC.mat','resultRMC')
-            save(sprintf('ml_yrmc_a_%d.mat',m),'Yrmc','t')
+            save(sprintf('ml_yrmc_cv%d_mi%d.mat',ci,m),'Yrmc','t')
         end
     end          
 end 
-end
 
 if SMC
-muiter=[5e4,2.8e4,1e4,5000,1000,500,100,50];
-resultSMC=zeros(niter,length(cviter), length(muiter), 3, length(f));
-mu0=1;%sum(svd(Y));
+    muiter=[5e4,2.8e4,1e4,5000,1000,500,100,50];
+    resultSMC=zeros(length(cviter), length(muiter), 3, length(f));
+    mu0=1;%sum(svd(Y));
 
-for i=1:length(niter)
     for ci=1:length(cviter)
         cv=cviter(ci);
-        load(sprintf('../ml-100k/folds/ml_b.mat'));
+        load(sprintf('../ml-100k/folds/ml_%d.mat',ci));
         par.maxrank = min([d1,d2,par.maxrank]);
         fprintf('Size: %dX%d, p:%f, train:val:test::%d:%d:%d\n',d1,d2,cv,...
             length(yy),length(yy_val),length(yy_test));
@@ -88,23 +86,22 @@ for i=1:length(niter)
             t=toc;
             yest=Amap_MatComp(Ysmc,ii_train,Jcol);            
             k1=evalRanking(yy,yest,Jcol,f,K,th);          
-            resultSMC(i,ci,m,1,:)=k1;
+            resultSMC(ci,m,1,:)=k1;
             
             %validation            
             yest_val=Amap_MatComp(Ysmc,ii_val,Jcol_val);
             k2=evalRanking(yy_val,yest_val,Jcol_val,f,K,th);
-            resultSMC(i,ci,m,2,:)=k2;
+            resultSMC(ci,m,2,:)=k2;
                         
             %testing
             yest_test=Amap_MatComp(Ysmc,ii_test,Jcol_test);
             k3=evalRanking(yy_test,yest_test,Jcol_test,f,K,th);                
-            resultSMC(i,ci,m,3,:)= k3;  
+            resultSMC(ci,m,3,:)= k3;  
             
             fprintf('SMC  mu:%f. iter:%d, res:%f, ||X||_*:%f, t:%f\n', ...
                 mu, iter,res,sum(sum(Ysmc.U.^2)),t);
             save('ml_resultSMC.mat','resultSMC')
-            save(sprintf('ml_ysmc_b_%d.mat',m),'Ysmc','t')
+            save(sprintf('ml_ysmc_cv%d_mi%d.mat',ci,m),'Ysmc','t')
         end
     end          
 end 
-end
